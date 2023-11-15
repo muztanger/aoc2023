10863de37d882140bd3990526d015e9797a99c85
Johan Ostrand <muztanger@gmail.com>
2023-11-15 21:54:36 +0100

    2017: day3 part2 progress

--- js/2017/day03.js
+++ js/2017/day03.js
@@ -10,9 +10,14 @@ class Pos {
 }
     
 class Square {
-    constructor(pos, value) {
+    constructor(pos, value, ring) {
         this.pos = pos;
         this.value = value;
+        this.ring = ring;
+    }
+
+    toString() {
+        return `(${this.pos.toString()}, ${this.value}, ${this.ring})`;
     }
 }
 
@@ -54,4 +59,54 @@ function part1(input) {
 // console.log(part1(23));
 // console.log(part1(29));
 // console.log(part1(1024));
-console.log(part1('redacted'));
+// console.log(part1('redacted'));
+
+function part2(input) {
+    console.log('part2:' + input);
+    var num = parseInt(input);
+    var pos = new Pos(0, 0);
+    var count = 1
+    var dirs = [new Pos(0, -1), new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0)];
+    var dir = 0;
+    var squares = {};
+    var value = 1;
+    squares[pos.toString()] = new Square(pos, value, 0);
+    pos = new Pos(1,0);
+    value = 1;
+    var x = 1;
+    count = 2;
+    squares[pos.toString()] = new Square(pos, value, x);
+    while (value < num) {
+        if (count < 1000) {
+            console.log("count: " + count + " pos: " + pos.toString() + " dirs[dir]: " + dirs[dir] + " x: " + x);
+        }
+        pos = new Pos(pos.x + dirs[dir].x, pos.y + dirs[dir].y);
+        value = 0;
+        for (var i = -1; i <= 1; i++) {
+            for (var j = -1; j <=1; j++) {
+                var p = new Pos(pos.x + i, pos.y + j);
+                var s = squares[p.toString()];
+                if (s != undefined) {
+                    value = value + s.value;
+                }
+            }
+        }
+        squares[pos.toString()] = new Square(pos, value, x);
+        if (pos.x == x && pos.y == x) {
+            x = x + 1;
+            dir = 3;
+            pos = new Pos(pos.x + dirs[dir].x, pos.y + dirs[dir].y);
+            count = count + 1;
+            dir = 0;
+        } else if (Math.abs(pos.x) == x && Math.abs(pos.y) == x) {
+            dir = (dir + 1) % 4;
+        }
+        count = count + 1;
+    }
+    for (const [key, value] of Object.entries(squares)) {
+        console.log(key + " " + value.toString());
+    }
+    return value;
+}
+
+console.log(part2(806));
