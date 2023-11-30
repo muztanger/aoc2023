fe30eb428a134935baf8a46deeccaae6c06521f2
Johan Ostrand <muztanger@gmail.com>
2023-11-30 21:37:16 +0100

    Starting work for boilerplate for aoc 2023 something is incorrect...

---
+++ js/utilities.js
@@ -0,0 +1,34 @@
+/**
+ * Fetches the input from Advent of Code website.
+ * @returns {Promise<string>} The fetched input.
+ * @throws {Error} If failed to fetch the input.
+ */
+const fs = require('./node_modules/fs');
+const fetch = import('./node_modules/node-fetch');
+
+
+
+async function fetchInput() {
+    const sessionCookie = fs.readFileSync('../session_cookie.txt', 'utf8').trim();
+
+    const response = await fetch('https://adventofcode.com/2022/day/1/input', {
+        headers: {
+            cookie: `session=${sessionCookie}`
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch input: ${response.status} ${response.statusText}`);
+    }
+
+    const input = await response.text();
+    return input.trim();
+}
+
+fetchInput()
+  .then(input => {
+    console.log(input);
+  })
+  .catch(error => {
+    console.error(error);
+  });
