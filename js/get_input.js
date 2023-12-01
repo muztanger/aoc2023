041bebca905289e227bffb6910ae222d9ab480be
Johan Ostrand <muztanger@gmail.com>
2023-12-01 23:38:43 +0100

    get_input.js added

---
+++ js/get_input.js
@@ -0,0 +1,22 @@
+const ut = require('./utilities.js');
+const fs = require('node:fs');
+
+var day = new Date().getDate();
+const filePath = `js/2023/day${day.toString().padStart(2, '0')}.in`;
+
+async function writeInputToFile(filePath) {
+    const input = await ut.fetchInput(day);
+    fs.writeFile(filePath, input, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        } else {
+            console.log(`Written to ${filePath} successfully.`);
+        }
+    });
+}
+
+if (fs.existsSync(filePath)) {
+    console.log(`${filePath} already exists.`);
+} else {
+    writeInputToFile(filePath);
+}
