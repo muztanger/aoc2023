29d5479ae009d24be4e19485231fe30cf40f2d8b
Johan Ostrand <muztanger@gmail.com>
2023-12-04 09:14:45 +0100

    day04 tests and array for count

--- day04.js
+++ day04.js
@@ -13,18 +13,16 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 // input  = example;
 
-var count = new Map();
+var count = [];
 var scores = new Map();
 var cards = [];
 var part1 = 0;
 for (const line of input.split('\n')) {
-    // console.log(line);
     const cardId = parseInt(line.split(':')[0].split(/[\s]+/)[1].trim());
-    // console.log(cardId);
     const data = line.split(':')[1];
     const win = new Set(data.split('|')[0].trim().split(/[\s]+/).map(Number));
     const my = new Set(data.split('|')[1].trim().split(/[\s]+/).map(Number));
-    count.set(cardId, 1);
+    count[cardId] = 1;
     var m = [...my].filter((x) => win.has(x));
     var score = m.reduce((a, b) => a * 2, 1);
     score = Math.floor(score / 2);
@@ -32,30 +30,29 @@ for (const line of input.split('\n')) {
     cards.push(cardId);
     part1 += score;
 }
-console.log(part1);
 
 var part2 = 0;
-while (Array.from(count.values()).some((x) => x > 0)) {
+while (count.some((x) => x > 0)) {
     for (const card of cards) {
-        if (count.get(card) == 0) continue;
-        // console.log(card, count.get(card), scores.get(card));
+        if (count[card] == 0) continue;
         var [score, len] = scores.get(card);
-        // part2 += score;
-
-        count.set(card, count.get(card) - 1);
+        count[card]--;
         part2++;
 
-        // if (len == 0) continue;
-        // console.log(len);
         for (var i = card + 1; i <= card + len; i++) {
             if (i > cards.length) {
                 console.error('i', i, 'len', len, 'card', card);
                 break;
             }
-            count.set(i, count.get(i) + 1);
+            count[i]++;
         }
-        // console.log(card, count.get(card), scores.get(card));
     }
-    // console.log(count);
 }
-console.log(part2);
+
+test('part1', () => {
+    assert.strictEqual(part1, 24160);
+});
+
+test('part2', () => {
+    assert.strictEqual(part2, 5659035);
+});
