2825fd31eb5a6bf02820f97acc5040d383571ad2
Johan Ostrand <muztanger@gmail.com>
2023-12-13 22:20:01 +0100

    day13 part 2 examples work...

--- day13.js
+++ day13.js
@@ -24,9 +24,7 @@ const example = `#.##..##.
 
 let grids = input.split('\n\n').map(grid => grid.split('\n').map(line => line.split('')));
 
-let part1 = 0;
-for (const grid of grids) {
-    // check horizontal reflection
+function checkHorizontalReflection(grid) {
     let hReflection = -1;
     for (let i = 1; i < grid.length; i++) {
         let r1 = i - 1;
@@ -54,7 +52,10 @@ for (const grid of grids) {
             break;
         }
     }
-    // check vertical reflection
+    return hReflection;
+}
+
+function checkVerticalReflection(grid) {
     let vReflection = -1;
     for (let i = 1; i < grid[0].length; i++) {
         let c1 = i - 1;
@@ -82,16 +83,71 @@ for (const grid of grids) {
             break;
         }
     }
-    // console.log("hReflection", hReflection, "vRflection", vReflection);
+    return vReflection;
+}
+
+let reflections = [];
+let part1 = 0;
+for (const grid of grids) {
+    let hReflection = checkHorizontalReflection(grid);
     if (hReflection > 0) {
         part1 += 100 * hReflection;
     }
+    let vReflection = checkVerticalReflection(grid);
     if (vReflection > 0) {
         part1 += vReflection;
     }
+    reflections.push([hReflection, vReflection]);
 }
 
 console.log("Part 1:", part1);
 test('Part 1', () => {
     assert.strictEqual(part1, 26957);
 });
+
+function smudge(grid, i, j) {
+    var result = [];
+    grid.forEach((row, rowIndex) => {
+        let newRow = [];
+        row.forEach((cell, colIndex) => {
+            if (rowIndex === j && colIndex === i) {
+                newRow.push(cell === '#' ? '.' : '#');
+            } else {
+                newRow.push(cell);
+            }
+        });
+        result.push(newRow);
+    });
+    return result;
+}
+
+let part2 = 0;
+for (let index = 0; index < grids.length; index++) {
+    const grid = grids[index];
+    const orig = reflections[index];
+    found = [];
+    for (let i = 0; i < grid[0].length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            let smudged = smudge(grid, i, j);
+            var hReflection = checkHorizontalReflection(smudged);
+            var vReflection = checkVerticalReflection(smudged);
+            // if (!found.some(xs => xs[0] === hReflection && xs[1] === vReflection)) {
+                found.push([hReflection, vReflection])
+            // }
+        }
+    }
+    for (let i = 0; i < found.length; i++) {
+        let hReflection = found[i][0];
+        if (hReflection > 0 && hReflection !== orig[0]) {
+            part2 += 100 * hReflection;
+        }
+        let vReflection = found[i][1];
+        if (vReflection > 0 && vReflection !== orig[1]) {
+            part2 += vReflection;
+        }
+    }
+}
+// 30551 is too low
+// 39178 is too low
+// 57591 is too high
+console.log("Part 2:", part2);
