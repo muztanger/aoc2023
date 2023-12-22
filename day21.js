c6a4c76dad84dce0e8a7fd6ebd0a4b163a4d7762
Johan Ostrand <muztanger@gmail.com>
2023-12-22 06:48:53 +0100

    day21 work

--- day21.js
+++ day21.js
@@ -104,3 +104,57 @@ test('part1', () => {
     assert.strictEqual(3830, part1);
 });
 
+function gridValue(pos) {
+    let p = new Pos(pos.x, pos.y);
+    while (p.x < 0) {
+        p.x += G[0].length;
+    }
+    p.x = p.x % G[0].length;
+    while (p.y < 0) {
+        p.y += G.length;
+    }
+    p.y = p.y % G.length;
+    return G[p.y][p.x];
+}
+
+function step2(starts) {
+    let found = new Set();
+    let result = [];
+    for (let pos of starts) {
+        for (let k of Object.keys(dirs)) {
+            let newPos = pos.add(dirs[k]);
+            
+            if (gridValue(newPos) !== '#' && !found.has(newPos.toString())) {
+                found.add(newPos.toString());
+                result.push(newPos);
+            }
+        }
+    }
+    return result;
+}
+
+let points = [start];
+points = step2(points);
+steps = 1;
+let part2 = points.length;
+var seen = [];
+var results = [];
+let moop = 0;
+for (let s = 1; s < 26501365; s++) {
+    points = step2(points);
+    steps++;
+    part2 = points.length;
+    console.log(s, part2);
+    let reachedCount = reached.size;
+    if (seen.includes(reachedCount)) {
+        break;
+    }
+    seen.push(reachedCount);
+    results.push(part2);
+    if (seen.length > 2) {
+        seen.shift();
+    }
+}
+// 15567 is too low
+console.log(results.join(','));
+part2 = results[results.length - 1];
