9aca9c5cf9fc11ea466f865c2201b675066b898d
Johan Ostrand <muztanger@gmail.com>
2024-01-05 13:58:54 +0100

    Day15 two stars!

--- day15.js
+++ day15.js
@@ -22,10 +22,61 @@ function part1(input) {
     return result;
 }
 
+
 test('part1', () => {
     assert.strictEqual(part1('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'), 1320);
-    assert.strictEqual(part1(input), -1);
+    assert.strictEqual(part1(input), 517551);
 })
+
+function part2(input) {
+    const operRegex = /(\w+)([-=])(\d*)/;
+    let boxes = Array(256).fill().map(() => []);
+    for (let step of input.split(',')) {
+        let [_, label, oper, value] = operRegex.exec(step);
+        let boxIndex = hash(label);
+        let box = boxes[boxIndex];
+        if (oper === '-') {
+            for (let i = 0; i < boxes.length; i++) {
+                let indexOf = boxes[i].map(([x, y]) => x).indexOf(label);
+                if (indexOf >= 0) {
+                    boxes[i].splice(indexOf, 1);
+                    // console.log(`Removed ${label} from box ${i} => ${boxes[i].map(([x, y]) => `[${x} ${y}]`).join(' ')}`);
+                    break;
+                }
+            }
+        } else if (oper === '=') {
+            let labelIndex = box.map(([x, y]) => x).indexOf(label);
+            if (labelIndex >= 0) {
+                box[labelIndex][1] = value;
+            } else {
+                box.push([label, value]);
+            }
+        }
+        // console.log("After: ", step);
+        // for (let i = 0; i < boxes.length; i++) {
+        //     if(boxes[i].length > 0) {
+        //         console.log(`Box ${i}: ${boxes[i].map(([x, y]) => `[${x} ${y}]`).join(' ')}`);
+        //     }
+        // }
+        // console.log();
+    }
+    let result = 0;
+    let b = 1;
+    for (let box of boxes) {
+        let slot = 1;
+        for (let [label, value] of box) {
+            result += b * slot * value;
+            slot++;
+        }
+        b++;
+    }
+    return result;
+}
+
+test('part2', () => {
+    assert.strictEqual(part2('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'), 145);
+    assert.strictEqual(part2(input), 286097);
+});
 
 
 test('hash', () => {
