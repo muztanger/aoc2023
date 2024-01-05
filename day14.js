856c1358cd2d36ab0c3617ce3537df13fec0c61d
Johan Ostrand <muztanger@gmail.com>
2024-01-05 08:39:31 +0100

    Day14 Two stars!
    Note to self. Think more about the datastructures!

--- day14.js
+++ day14.js
@@ -76,161 +76,177 @@ class Rock {
     }
 }
 
-let rocks = [];
-let roll = null;
-let area = new Area();
-
-function readInput() {
-    area = new Area();
-    rocks = [];
-    let y = 0;
-    for (let row of input.split('\n')) {
-        let x = 0;
-        for (let cell of row.split('')) {
-            if (cell === '#' || cell === 'O') {
-                rocks.push(new Rock(new Pos(x, y), cell));
-            }
-            area.expand(new Pos(x, y));
-            x++;
+class RockGrid {
+    static types = ['.', '#', 'O'];
+    constructor(rocks, area) {
+        this.area = area;
+        this.grid = Array(area.max.y - area.min.y + 1).fill().map(() => Array(area.max.x - area.min.x + 1).fill(0));
+        for (let rock of rocks) {
+            this.grid[rock.pos.y - area.min.y][rock.pos.x - area.min.x] = RockGrid.types.indexOf(rock.type);
         }
-        y++;
     }
-    roll = rocks.filter(rock => rock.type === 'O');
-}
-readInput();
 
-console.log('Area:', area.toString());
+    static fromInput(input) {
+        let rocks = [];
+        let area = new Area();
+        let y = 0;
+        for (let row of input.split('\n')) {
+            let x = 0;
+            for (let cell of row.split('')) {
+                if (cell === '#' || cell === 'O') {
+                    rocks.push(new Rock(new Pos(x, y), cell));
+                }
+                area.expand(new Pos(x, y));
+                x++;
+            }
+            y++;
+        }
+        return new RockGrid(rocks, area);
+    }
+    
+    get(pos) {
+        if (this.area.contains(pos)) {
+            return this.grid[pos.y - this.area.min.y][pos.x - this.area.min.x];
+        }
+        return null;
+    }
 
-const printRocks = (rocks) => {
-    if (!useExample) return;
-
-    for (let y = area.min.y; y <= area.max.y; y++) {
-        let row = '';
-        for (let x = area.min.x; x <= area.max.x; x++) {
-            let rock = rocks.find(rock => rock.pos.equals(new Pos(x, y)));
-            if (rock) {
-                row += rock.type;
-            } else {
-                row += '.';
+    tiltNorth() {
+        for (let x = this.area.min.x; x <= this.area.max.x; x++) {
+            let lastSolidY = this.area.min.y - 1;
+            for (let y = this.area.min.y; y <= this.area.max.y; y++) {
+                let rock = this.get(new Pos(x, y));
+                if (rock > 0) {
+                    if (y - lastSolidY > 1 && rock === 2) {
+                        this.grid[y - this.area.min.y][x - this.area.min.x] = 0;
+                        this.grid[lastSolidY - this.area.min.y + 1][x - this.area.min.x] = 2;
+                        lastSolidY++;
+                    } else {
+                        lastSolidY = y;
+                    }
+                }
             }
         }
-        console.log(row);
+    }
+
+    tiltSouth() {
+        for (let x = this.area.min.x; x <= this.area.max.x; x++) {
+            let lastSolidY = this.area.max.y + 1;
+            for (let y = this.area.max.y; y >= this.area.min.y; y--) {
+                let rock = this.get(new Pos(x, y));
+                if (rock > 0) {
+                    if (lastSolidY - y > 1 && rock === 2) {
+                        this.grid[y - this.area.min.y][x - this.area.min.x] = 0;
+                        this.grid[lastSolidY - this.area.min.y - 1][x - this.area.min.x] = 2;
+                        lastSolidY--;
+                    } else {
+                        lastSolidY = y;
+                    }
+                }
+            }
+        }
+    }
+
+    tiltEast() {
+        for (let y = this.area.min.y; y <= this.area.max.y; y++) {
+            let lastSolidX = this.area.max.x + 1;
+            for (let x = this.area.max.x; x >= this.area.min.x; x--) {
+                let rock = this.get(new Pos(x, y));
+                if (rock > 0) {
+                    if (lastSolidX - x > 1 && rock === 2) {
+                        this.grid[y - this.area.min.y][x - this.area.min.x] = 0;
+                        this.grid[y - this.area.min.y][lastSolidX - this.area.min.x - 1] = 2;
+                        lastSolidX--;
+                    } else {
+                        lastSolidX = x;
+                    }
+                }
+            }
+        }
+    }
+
+    tiltWest() {
+        for (let y = this.area.min.y; y <= this.area.max.y; y++) {
+            let lastSolidX = this.area.min.x - 1;
+            for (let x = this.area.min.x; x <= this.area.max.x; x++) {
+                let rock = this.get(new Pos(x, y));
+                if (rock > 0) {
+                    if (x - lastSolidX > 1 && rock === 2) {
+                        this.grid[y - this.area.min.y][x - this.area.min.x] = 0;
+                        this.grid[y - this.area.min.y][lastSolidX - this.area.min.x + 1] = 2;
+                        lastSolidX++;
+                    } else {
+                        lastSolidX = x;
+                    }
+                }
+            }
+        }
+    }
+
+    cycle() {
+        this.tiltNorth();
+        this.tiltWest();
+        this.tiltSouth();
+        this.tiltEast();
+    }
+
+    totalLoad() {
+        let total = 0;
+        for (let y = this.area.min.y; y <= this.area.max.y; y++) {
+            for (let x = this.area.min.x; x <= this.area.max.x; x++) {
+                if (this.get(new Pos(x, y)) === 2) {
+                    total += this.area.max.y - y + 1;
+                }
+            }
+        }
+        return total;
+    }
+
+    print() {
+        for (let y = this.area.min.y; y <= this.area.max.y; y++) {
+            let row = '';
+            for (let x = this.area.min.x; x <= this.area.max.x; x++) {
+                let rock = this.get(new Pos(x, y));
+                if (rock) {
+                    row += RockGrid.types[rock];
+                } else {
+                    row += '.';
+                }
+            }
+            console.log(row);
+        }
+    }
+
+    getState() {
+        let state = '';
+        for (let y = this.area.min.y; y <= this.area.max.y; y++) {
+            for (let x = this.area.min.x; x <= this.area.max.x; x++) {
+                let rock = this.get(new Pos(x, y));
+                if (rock) {
+                    state += RockGrid.types[rock];
+                } else {
+                    state += '.';
+                }
+            }
+        }
+        return state.hashCode();
     }
 }
 
-printRocks(rocks);
-
-let rockMap = new Map();
-for (let key in rocks) {
-    rockMap.set(rocks[key].pos.toString(), rocks[key]);
+console.log('--- Part 1 ---');
+const rockGridPart1 = RockGrid.fromInput(input);
+if (useExample) {
+    rockGridPart1.print();
+    console.log();
 }
-const tilt = (dir) => {
-    // if (useExample) {
-    //     console.log('Tilt', dir);
-    //     console.log('Before tilt:');
-    //     printRocks(rocks);
-    // }
-    switch (dir) {
-        case 'N':
-            {
-                let nextRockMap = new Map();
-                for (let x = area.min.x; x <= area.max.x; x++) {
-                    let lastSolidY = area.min.y - 1;
-                    for (let y = area.min.y; y <= area.max.y; y++) {
-                        let rock = rockMap.get(new Pos(x, y).toString());
-                        if (rock) {
-                            if (y - lastSolidY > 1 && rock.type === 'O') {
-                                rock.pos.y = lastSolidY + 1;
-                            }
-                            nextRockMap.set(rock.pos.toString(), rock);
-                            lastSolidY = rock.pos.y;
-                        }
-                    }
-                }
-                rockMap = nextRockMap;
-            }
-            break;
-        case 'S':
-            {
-                let nextRockMap = new Map();
-                for (let x = area.min.x; x <= area.max.x; x++) {
-                    let lastSolidY = area.max.y + 1;
-                    for (let y = area.max.y; y >= area.min.y; y--) {
-                        let rock = rockMap.get(new Pos(x, y).toString());
-                        if (rock) {
-                            if (lastSolidY - y > 1 && rock.type === 'O') {
-                                rock.pos.y = lastSolidY - 1;
-                            }
-                            nextRockMap.set(rock.pos.toString(), rock);
-                            lastSolidY = rock.pos.y;
-                        }
-                    }
-                }
-                rockMap = nextRockMap;
-            }
-            break;
-        case 'E':
-            {
-                let nextRockMap = new Map();
-                for (let y = area.min.y; y <= area.max.y; y++) {
-                    let lastSolidX = area.max.x + 1;
-                    for (let x = area.max.x; x >= area.min.x; x--) {
-                        let rock = rockMap.get(new Pos(x, y).toString());
-                        if (rock) {
-                            if (lastSolidX - x > 1 && rock.type === 'O') {
-                                rock.pos.x = lastSolidX - 1;
-                            }
-                            nextRockMap.set(rock.pos.toString(), rock);
-                            lastSolidX = rock.pos.x;
-                        }
-                    }
-                }
-                rockMap = nextRockMap;
-            }
-            break;
-        case 'W':
-            {
-                let nextRockMap = new Map();
-                for (let y = area.min.y; y <= area.max.y; y++) {
-                    let lastSolidX = area.min.x - 1;
-                    for (let x = area.min.x; x <= area.max.x; x++) {
-                        let rock = rockMap.get(new Pos(x, y).toString());
-                        if (rock) {
-                            if (x - lastSolidX > 1 && rock.type === 'O') {
-                                rock.pos.x = lastSolidX + 1;
-                            }
-                            nextRockMap.set(rock.pos.toString(), rock);
-                            lastSolidX = rock.pos.x;
-                        }
-                    }
-                }
-                rockMap = nextRockMap;
-            }
-            break;
-        default:
-            break;
-    }
-    // if (useExample) {
-    //     console.log('After tilt:');
-    //     printRocks(rocks);
-    //     console.log();
-    // }
+rockGridPart1.tiltNorth();
+if (useExample) {
+    console.log('After tilting north:');
+    rockGridPart1.print();
 }
 
-tilt('N');
-console.log('After gravity:');
-printRocks(rocks);
-
-const totalLoad = () => {
-    let total = 0;
-    for (let rock of roll) {
-        total += area.max.y - rock.pos.y + 1;
-    }
-    return total;
-}
-
-let part1 = totalLoad();
-console.log('Part 1:', part1);
+let part1 = rockGridPart1.totalLoad();
+console.log('Part 1 result:', part1);
 
 test ('Part 1', () => {
     if (useExample) {
@@ -241,20 +257,7 @@ test ('Part 1', () => {
 });
 
 console.log('Part 2:');
-readInput();
-rockMap = new Map();
-for (let key in rocks) {
-    rockMap.set(rocks[key].pos.toString(), rocks[key]);
-}
-console.log('area:', area.toString());
-printRocks(rocks);
-
-function cycle() {
-    tilt('N');
-    tilt('W');
-    tilt('S');
-    tilt('E');
-}
+const rockGridPart2 = RockGrid.fromInput(input);
 
 let start = performance.now();
 let cycleTimes = [];
@@ -262,13 +265,8 @@ let mem = new Map();
 let loop = [];
 let loopStart = -1;
 let index = 0;
-function getState() {
-    // return rocks.filter(r => r.type == 'O').map(rock => rock.pos.toString()).join(';');
-    // return totalLoad();
-    return rocks.filter(r => r.type == 'O').map(rock => rock.pos.toString()).join(';').hashCode();
-}
 for (;;) {
-    const state = getState();
+    const state = rockGridPart2.getState();
     if (mem.has(state)) {
         console.log('Found loop');
         if (loopStart < 0) {
@@ -282,9 +280,9 @@ for (;;) {
         }
         break;
     } else {
-        const currentLoad = totalLoad();
-        cycle();
-        const nextState = getState();
+        const currentLoad = rockGridPart2.totalLoad();
+        rockGridPart2.cycle();
+        const nextState = rockGridPart2.getState();
         mem.set(state, {'load':currentLoad, 'next': nextState});
     }
 
@@ -295,7 +293,7 @@ for (;;) {
         console.log(`After ${c} cycles: load=${totalLoad()} totalTime=${now - start} ms avgTime=${(now - start) / c} ms`);
     }
     if (useExample) {
-        printRocks(rocks);
+        rockGridPart2.print();
         console.log();
     }
     index++;
@@ -319,6 +317,6 @@ test('Part 2', () => {
         assert.notEqual(part2, 96994); // incorrect answer
         assert.notEqual(part2, 97044); // incorrect answer
         assert.notEqual(part2, 97080); // incorrect answer
-        assert.equal(part2, -1);
+        assert.equal(part2, 97241);
     }
 });
