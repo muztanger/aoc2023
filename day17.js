5d23b58b631a2d33e1ca03514e62324d0477a4df
Johan Ostrand <muztanger@gmail.com>
2024-01-11 13:10:30 +0100

    Day17 major speedup by borrowing heap based priority queue

--- day17.js
+++ day17.js
@@ -43,29 +43,28 @@ class Pos {
     }
 }
 
+/* MinHeap minimised - taken from https://stackoverflow.com/a/66511107/5459839 */
+const MinHeap={siftDown(h,i=0,v=h[i]){if(i<h.length){let k=v[0];while(1){let j=i*2+1;if(j+1<h.length&&h[j][0]>h[j+1][0])j++;if(j>=h.length||k<=h[j][0])break;h[i]=h[j];i=j;}h[i]=v}},heapify(h){for(let i=h.length>>1;i--;)this.siftDown(h,i);return h},pop(h){return this.exchange(h,h.pop())},exchange(h,v){if(!h.length)return v;let w=h[0];this.siftDown(h,0,v);return w},push(h,v){let k=v[0],i=h.length,j;while((j=(i-1)>>1)>=0&&k<h[j][0]){h[i]=h[j];i=j}h[i]=v;return h}};
 class PriorityQueue {
     constructor() {
-        this.queue = [];
+        this.heap = [];
     }
-    enqueue(item, priority) {
-        this.queue.push({item, priority});
-        this.queue.sort((a, b) => a.priority - b.priority);
+    enqueue(value, priority) {
+        MinHeap.push(this.heap, [priority, value]);
     }
     dequeue() {
-        if (this.isEmpty()) {
+        if (this.heap.length === 0) {
             return null;
         }
-        return this.queue.shift().item;
+        return MinHeap.pop(this.heap)[1];
     }
-    isEmpty() {
-        return this.queue.length === 0;
-    }
-    get length () {
-        return this.queue.length;
+    get length() {
+        return this.heap.length;
     }
 }
 
 test('PriorityQueue', () => {
+    let heap = [];
     let queue = new PriorityQueue();
     queue.enqueue('a', 3);
     queue.enqueue('b', 2);
@@ -81,8 +80,8 @@ test('PriorityQueue', () => {
     queue.enqueue('d', 2);
     queue.enqueue('e', 3);
     assert.strictEqual(queue.dequeue(), 'c');
-    assert.strictEqual(queue.dequeue(), 'b');
-    assert.strictEqual(queue.dequeue(), 'd');
+    assert.strictEqual(['b', 'd'].includes(queue.dequeue()), true);
+    assert.strictEqual(['b', 'd'].includes(queue.dequeue()), true);
     assert.strictEqual(queue.dequeue(), 'a');
     assert.strictEqual(queue.dequeue(), 'e');
 
@@ -148,9 +147,9 @@ test('17-1 example', () => {
     assert.strictEqual(part1(example), 102);
 });
 
-// test('17-1', () => {
-//     assert.strictEqual(part1(input), 638);
-// });
+test('17-1', () => {
+    assert.strictEqual(part1(input), 638);
+});
 
 function part2(input) {
     const G = input.split('\n').map(line => line.split('').map(c => parseInt(c)));
@@ -184,13 +183,17 @@ function part2(input) {
             let dp = state.pos.add([LEFT, DOWN, RIGHT, UP][i]); 
 
             if (dp.x >= 0 && dp.x < C && dp.y >= 0 && dp.y < R) { // can't go off the grid
+                let nextCount = i == state.direction ? state.count + 1 : 1;
+                const nextKey = dp.toString() + ',' + i + ',' + nextCount;
+                if (D.has(nextKey)) { // already been here
+                    continue;
+                }
                 let nextEnergy = state.energy + G[dp.y][dp.x];
                 if ((i == state.direction || state.direction < 0) && state.count < 4) { // need to keep going straight
-                    queue.enqueue({ pos: dp, direction: i, count: state.count + 1, energy: nextEnergy }, nextEnergy);
+                    queue.enqueue({ pos: dp, direction: i, count: nextCount, energy: nextEnergy }, nextEnergy);
                     continue;
                 }
                 if (state.count >= 4 && state.count <= 10) { // we can go straight again or turn
-                    let nextCount = i == state.direction ? state.count + 1 : 1;
                     queue.enqueue({ pos: dp, direction: i, count: nextCount, energy: nextEnergy }, nextEnergy);
                 }
             }
